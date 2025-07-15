4ad3ebc3083aa3ed500e881269cef6dd10dd3c95
Aniket Mishra <aniket.mishra200510@gmail.com>
2025-07-15 13:05:19 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,9 +6,21 @@ def setup_page():
         layout="centered",
         initial_sidebar_state="auto"
     )
+    _center_page_layout()
     with st.sidebar:
-        #st.image("assets/logo.png", width=150)  # Remove if no logo
         st.markdown("## 🔭 Navigation")
         st.markdown("Use the menu on the left to explore different views.")
 
-
+def _center_page_layout():
+    st.markdown("""
+        <style>
+        @media (min-width: 768px) {
+            .main .block-container {
+                max-width: 700px;
+                margin-left: auto;
+                margin-right: auto;
+                padding-top: 2rem;
+            }
+        }
+        </style>
+    """, unsafe_allow_html=True)
